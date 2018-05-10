6bee01b8fb232acff4c803cba18ea1d49ca62d3d
ehmicky <ehmicky@gmail.com>
2018-05-10 17:00:00 +0200

    Improve shortcut notations

--- src/tests/merge.js
+++ src/tests/merge.js
@@ -2,6 +2,8 @@
 
 const { merge } = require('lodash')
 
+const { isObject } = require('../utils')
+
 // Deep merge parameters or headers with the same name
 const mergeInputs = function({ inputs }) {
   return inputs.map(mergeInput).filter(input => input !== undefined)
@@ -32,22 +34,40 @@ const isSameInput = function({ inputA, inputB }) {
 }
 
 const mergeSingleInput = function(inputA, inputB) {
-  // `test.request|response.*: true` means we re-use parameter's schema
-  if (inputB.schema === true) {
-    return inputA
-  }
-
-  // `test.request|response.*: false` means we inverse re-use parameter's schema
-  if (inputB.schema === false) {
+  // `test.request|response.*: invalid` means we inverse re-use parameter's schema
+  if (inputB.schema === 'invalid') {
     return { ...inputA, schema: { not: inputA.schema } }
   }
 
-  // `test.request|response.*: undefined|null` means we do not use that parameter
-  if (inputB.schema == null) {
-    return
+  // `test.request|response.*: non-object` is shortcut for `{ enum: [value] }`
+  if (!isObject(inputB.schema)) {
+    const type = getSchemaType({ value: inputB.schema })
+    return { ...inputA, schema: { ...inputA.schema, type, enum: [inputB.schema] } }
   }
 
+  // Otherwise it is a JSON schema that we deep merge
   return merge({}, inputA, inputB)
+}
+
+// When using the shortcut notation, we need to set the `type` to make sure it
+// matches the value (in case it is not set, or set to several types, or set to
+// a different type)
+const getSchemaType = function({ value }) {
+  if (typeof value === 'string') {
+    return 'string'
+  }
+
+  if (Number.isInteger(value)) {
+    return 'integer'
+  }
+
+  if (typeof value === 'number') {
+    return 'number'
+  }
+
+  if (Array.isArray(value)) {
+    return 'array'
+  }
 }
 
 module.exports = {

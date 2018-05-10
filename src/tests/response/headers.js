9b2e2d9e35ac34c5207a09859d0e463e9f6e98e6
ehmicky <ehmicky@gmail.com>
2018-05-10 17:00:00 +0200

    Rename test.response.headers.NAME to test.response['headers.name']

--- src/tests/response/headers.js
+++ src/tests/response/headers.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const { mapKeys, mapValues, merge } = require('lodash')
+const { mapKeys, mapValues, merge, pickBy } = require('lodash')
 
 const { addContentNegotiationsHeaders } = require('../content_negotiation')
 const { normalizeSchema } = require('../json_schema')
@@ -9,13 +9,14 @@ const { normalizeSchema } = require('../json_schema')
 const getSpecResHeaders = function({
   headers = {},
   operationObject: { consumes, produces },
-  testOpts: { response: { headers: testHeaders } = {} },
+  testOpts: { response },
 }) {
   const headersA = normalizeHeaders({ headers })
-  const testHeadersA = normalizeHeaders({ headers: testHeaders })
 
-  // Deep merge `response.headers` and `testOpts.response.headers`
-  const headersB = merge({}, headersA, testHeadersA)
+  const testHeaders = getTestHeaders({ response })
+
+  // Deep merge `response.headers` and `test.response.headers`
+  const headersB = merge({}, headersA, testHeaders)
 
   const headersC = arrifyHeaders({ headers: headersB })
 
@@ -23,6 +24,28 @@ const getSpecResHeaders = function({
 
   return headersD
 }
+
+// We use the `test.response['headers.NAME']` notation instead of
+// `test.response.headers.NAME` because it aligns headers with other properties
+// of the same nesting level. It also prevents too much nesting, which makes
+// the file looks more complicated than it is
+const getTestHeaders = function({ response = {} }) {
+  const testHeaders = pickBy(response, isTestHeader)
+  const testHeadersA = mapKeys(testHeaders, removePrefix)
+  const testHeadersB = normalizeHeaders({ headers: testHeadersA })
+  return testHeadersB
+}
+
+const isTestHeader = function(value, name) {
+  return name.startsWith(HEADERS_PREFIX)
+}
+
+const removePrefix = function(value, name) {
+  return name.replace(HEADERS_PREFIX, '')
+}
+
+// We use `test.response['headers.NAME']` notation
+const HEADERS_PREFIX = 'headers.'
 
 const normalizeHeaders = function({ headers }) {
   const headersA = mapKeys(headers, (_, name) => name.toLowerCase())
@@ -37,11 +60,7 @@ const normalizeHeader = function({ collectionFormat, ...schema }) {
 }
 
 const arrifyHeaders = function({ headers }) {
-  return Object.entries(headers).map(([name, { schema, collectionFormat }]) => ({
-    name,
-    schema,
-    collectionFormat,
-  }))
+  return Object.entries(headers).map(([name, header]) => ({ ...header, name }))
 }
 
 module.exports = {

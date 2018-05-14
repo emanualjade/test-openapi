6f829c80a8b7d859173e488191418744d3bfdaf6
ehmicky <ehmicky@gmail.com>
2018-05-14 17:00:00 +0200

    Fix title padding

--- src/tests/title.js
+++ src/tests/title.js
@@ -1,7 +1,5 @@
 'use strict'
 
-const { mapValues } = require('lodash')
-
 // `it()` title
 const addTitles = function({ tests }) {
   const paddings = getPaddings({ tests })
@@ -11,10 +9,8 @@ const addTitles = function({ tests }) {
 
 // Returns minimal padding needed for each test property used in test titles
 const getPaddings = function({ tests }) {
-  return mapValues(PADDINGS, (_, name) => getPadding({ name, tests }))
+  return PADDINGS.map(name => getPadding({ name, tests }))
 }
-
-const PADDINGS = { method: 0, path: 0 }
 
 const getPadding = function({ name, tests }) {
   const values = tests.map(({ operation }) => operation[name].length)
@@ -23,18 +19,21 @@ const getPadding = function({ name, tests }) {
 }
 
 const addTitle = function({ test, test: { name }, paddings }) {
-  const [method, path] = addPaddings({ names: ['method', 'path'], test, paddings })
+  const [method, path] = addPaddings({ test, paddings })
   const title = `${method} ${path} (${name})`
   return { ...test, title }
 }
 
-const addPaddings = function({ names, test, paddings }) {
-  return names.map(name => addPadding({ name, test, paddings }))
+const addPaddings = function({ test, paddings }) {
+  return paddings.map((padding, index) => addPadding({ padding, test, index }))
 }
 
-const addPadding = function({ name, test: { operation }, paddings }) {
-  return operation[name].padEnd(paddings[name])
+const addPadding = function({ padding, test: { operation }, index }) {
+  const name = PADDINGS[index]
+  return operation[name].padEnd(padding)
 }
+
+const PADDINGS = ['method', 'path']
 
 module.exports = {
   addTitles,

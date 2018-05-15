a288c0b1a057a26460992ec6e5af372ff245a771
ehmicky <ehmicky@gmail.com>
2018-05-15 17:00:00 +0200

    Do not pass shortcut options down

--- src/cli/parse.js
+++ src/cli/parse.js
@@ -1,13 +1,15 @@
 'use strict'
 
-const { omit } = require('lodash')
+const { omit, omitBy } = require('lodash')
 
 const parseOpts = function({ yargs }) {
   const { _: posOpts, ...opts } = yargs.parse()
   // `yargs`-specific options
   const optsA = omit(opts, ['help', 'version', '$0'])
+  // Remove shortcuts
+  const optsB = omitBy(optsA, (value, name) => name.length === 1)
   const tests = posOpts.length === 0 ? undefined : posOpts
-  return { ...optsA, tests }
+  return { ...optsB, tests }
 }
 
 module.exports = {

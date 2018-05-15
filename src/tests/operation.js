a98f2de703446d3f0f389cc6e6e273c046aeb442
ehmicky <ehmicky@gmail.com>
2018-05-15 17:00:00 +0200

    Validate test operation names

--- src/tests/operation.js
+++ src/tests/operation.js
@@ -1,17 +1,31 @@
 'use strict'
 
+const { throwTestError } = require('../errors')
 const { DEFAULT_STATUS_CODE } = require('../constants')
 
 // Find the operation related to a specific test, and add it
 // Does so by checking test key which should be `OperationId.testName`
-const getOperation = function({ testKey, testOpts, spec: { operations } }) {
-  const operation = operations.find(({ operationId = '' }) => testKey.startsWith(`${operationId}.`))
+const getOperation = function({ testKey, testOpts, spec }) {
+  const operation = findOperation({ testKey, spec })
   const name = testKey.replace(`${operation.operationId}.`, '')
 
   const response = findResponse({ operation, testOpts })
   const operationA = { ...operation, response }
 
   return { name, operation: operationA }
+}
+
+const findOperation = function({ testKey, spec: { operations } }) {
+  const operation = operations
+    .filter(({ operationId }) => operationId)
+    .find(({ operationId }) => testKey.startsWith(`${operationId}.`))
+  if (operation !== undefined) {
+    return operation
+  }
+
+  throwTestError(`Test named '${testKey}' does not match any operation in the specification`, {
+    test: testKey,
+  })
 }
 
 // Find the operation's response related to a specific test, and add it

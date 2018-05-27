96a2b6c8f4a830905c095b1daa8c4f821a962b08
ehmicky <ehmicky@gmail.com>
2018-05-27 17:00:00 +0100

    Fix task return value

--- src/init/run.js
+++ src/init/run.js
@@ -1,13 +1,31 @@
 'use strict'
 
+const { mapValues } = require('lodash')
+
+const { isObject } = require('../utils')
+
 const { runHandlers } = require('./plugins')
 
 // Run an `it()` task
 const runTask = async function({ originalTask, ...task }, handlers) {
   const taskA = await runHandlers(task, handlers)
   // Task return value, returned to users and used by depReqs
-  // The originalTask properties cannot be overriden
-  return { ...taskA, ...originalTask }
+  const taskB = mapValues(taskA, (newTask, prop) => shallowMerge(originalTask[prop], newTask))
+  return taskB
+}
+
+// Do a shallow merge on each plugin value
+// The originalTask properties has less priority
+const shallowMerge = function(originalTask, newTask) {
+  if (newTask === undefined) {
+    return originalTask
+  }
+
+  if (!isObject(originalTask) || !isObject(newTask)) {
+    return newTask
+  }
+
+  return { ...originalTask, ...newTask }
 }
 
 module.exports = {

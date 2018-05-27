fe1d2bb4c96e741f6a979f1e551ab0363c60fb69
ehmicky <ehmicky@gmail.com>
2018-05-27 17:00:00 +0100

    Replace sway by swagger-parser

--- src/plugins/spec/load.js
+++ src/plugins/spec/load.js
@@ -1,49 +1,45 @@
 'use strict'
 
-const { dirname, basename } = require('path')
+const SwaggerParser = require('swagger-parser')
 
 const { addErrorHandler, throwSpecificationError } = require('../../errors')
 
-const { validateOpenApi } = require('./validate')
-
 // Parses an OpenAPI file (including JSON references)
-// Then validates its syntax
-const loadOpenApiSpec = async function({ path }) {
-  const spec = await eLoadSpec({ path })
-
-  validateOpenApi({ spec })
-
-  return spec.definitionFullyResolved
+// Can also be a URL or directly an object
+// Also validates its syntax
+const loadOpenApiSpec = function({ spec }) {
+  return SwaggerParser.validate(spec)
 }
 
-const loadSpec = async function({ path }) {
-  const definition = basename(path)
-  const relativeBase = dirname(path)
+const loadSpecHandler = function({ message, details }) {
+  if (!Array.isArray(details)) {
+    fetchSpecHandler({ message })
+  }
 
-  const sway = getSway()
-  const specA = await sway.create({ definition, jsonRefs: { relativeBase } })
-  return specA
+  invalidSpecHandler({ details })
 }
 
-const loadSpecHandler = function({ message }) {
+// Validate OpenAPI file exists and can be fetched
+const fetchSpecHandler = function({ message }) {
   throwSpecificationError(`OpenAPI specification could not be loaded: ${message}`)
 }
 
-const eLoadSpec = addErrorHandler(loadSpec, loadSpecHandler)
-
-// This is needed until https://github.com/whitlockjc/json-refs/pull/133
-// is merged.
-// Sway's dependency `json-refs` caches files, so when this function gets called
-// again, it might received old file contents.
-// This is problematic for Gulp tasks running in watch mode.
-const getSway = function() {
-  Object.keys(require.cache)
-    .filter(path => path.includes('/sway/') || path.includes('/json-refs/'))
-    .forEach(path => delete require.cache[path])
-  const sway = require('sway')
-  return sway
+// Validate OpenAPI specification syntax
+const invalidSpecHandler = function({ details, details: [{ path }] }) {
+  const message = details.map(getErrorMessage).join(`\n${INDENT}`)
+  const property = path.join('.')
+  throwSpecificationError(`OpenAPI specification is invalid:\n${INDENT}${message}`, { property })
 }
 
+const INDENT_LENGTH = 4
+const INDENT = ' '.repeat(INDENT_LENGTH)
+
+const getErrorMessage = function({ path, message }) {
+  return `At '${path.join('.')}': ${message}`
+}
+
+const eLoadOpenApiSpec = addErrorHandler(loadOpenApiSpec, loadSpecHandler)
+
 module.exports = {
-  loadOpenApiSpec,
+  loadOpenApiSpec: eLoadOpenApiSpec,
 }
--- src/plugins/spec/normalize/main.js
+++ src/plugins/spec/normalize/main.js
@@ -7,22 +7,19 @@ const { getOperations } = require('./operations')
 // Parses an OpenAPI file (including JSON references)
 // Then validates its syntax
 // Then normalize it
-const loadNormalizedSpec = async function({ spec: path }) {
-  if (path === undefined) {
+const loadNormalizedSpec = async function({ spec }) {
+  if (spec === undefined) {
     return
   }
 
-  const spec = await loadOpenApiSpec({ path })
+  const specA = await loadOpenApiSpec({ spec })
 
-  const specA = normalizeSpec({ spec })
+  const specB = normalizeSpec({ spec: specA })
 
-  return { spec: specA }
+  return { spec: specB }
 }
 
 // Normalize specification object
-// We do not use Sway for traversing as it has some problems, e.g. does not
-// properly parse security alternatives
-// Also it eases being specification-agnostic
 const normalizeSpec = function({ spec }) {
   const operations = getOperations({ spec })
   return { operations }

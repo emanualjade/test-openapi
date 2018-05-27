402681f80bbb090f5f4c8685cb27ed9ccfdb8c12
ehmicky <ehmicky@gmail.com>
2018-05-27 17:00:00 +0100

    Validate tasks are JSON

--- src/plugins/tasks/validate.js
+++ src/plugins/tasks/validate.js
@@ -1,6 +1,8 @@
 'use strict'
 
-const { throwTaskError } = require('../../errors')
+const { isDeepStrictEqual } = require('util')
+
+const { throwTaskError, addErrorHandler } = require('../../errors')
 const { validateFromSchema, isObject } = require('../../utils')
 
 const TASK_SCHEMA = require('./schema')
@@ -30,6 +32,28 @@ const validateEmptyTasks = function({ tasks }) {
 }
 
 const validateTask = function([taskKey, task]) {
+  eValidateJson({ taskKey, task })
+  validateTaskSchema({ taskKey, task })
+}
+
+// Tasks are constrained to JSON
+// This also validates against circular references
+const validateJson = function({ taskKey, task }) {
+  const copy = JSON.parse(JSON.stringify(task))
+  if (isDeepStrictEqual(task, copy)) {
+    return
+  }
+
+  throwTaskError(`Task '${taskKey}' is not valid JSON`, { task: taskKey })
+}
+
+const validateJsonHandler = function({ message }, { taskKey }) {
+  throwTaskError(`Task '${taskKey}' is not valid JSON: ${message}`, { task: taskKey })
+}
+
+const eValidateJson = addErrorHandler(validateJson, validateJsonHandler)
+
+const validateTaskSchema = function({ taskKey, task }) {
   const { error, path } = validateFromSchema({ schema: TASK_SCHEMA, value: task, name: taskKey })
   if (error === undefined) {
     return

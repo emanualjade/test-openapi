9807af7692bfc85ad18ceeb92a0aae1986a24cc8
ehmicky <ehmicky@gmail.com>
2018-05-27 17:00:00 +0100

    Refactor plugin firing

--- src/init/run.js
+++ src/init/run.js
@@ -29,44 +29,50 @@ const runTasks = async function({
   },
   tasks,
 }) {
-  const runningTasks = new Array(repeat).fill().map(() => runTask({ task, tasks }))
+  // Passed as second argument to every plugin
+  const secondArg = { tasks }
+
+  const runningTasks = new Array(repeat).fill().map(() => runTask({ task, secondArg }))
   await Promise.all(runningTasks)
 }
 
 const eRunTasks = addErrorHandler(runTasks, runTasksHandler)
 
 // Run an `it()` task
-const runTask = async function({ task, task: { originalTask }, tasks }) {
-  const {
-    task: { request, response },
-  } = await eRunPlugins({ task, tasks })
+const runTask = async function({ task: { originalTask, ...task }, secondArg }) {
+  const secondArgA = addRunTask({ secondArg })
+
+  const { task: taskA } = await eRunPlugins({ task, secondArg: secondArgA })
+  const { request, response } = taskA
   // `task` will be the initial value before any plugin transformation
   return { request, response, ...originalTask }
 }
 
-const runPlugins = function({ task, tasks }) {
-  return reduceAsync(PLUGINS, eRunPlugin, { task, tasks }, mergePlugin)
+// Pass `runTask` for recursive tasks
+const addRunTask = function({ secondArg }) {
+  const runTaskA = task => runTask({ task, secondArg })
+  return { ...secondArg, runTask: runTaskA }
 }
 
-const runPlugin = function({ task, tasks }, plugin) {
-  // Pass `runTask` for recursive tasks
-  const runTaskA = taskA => runTask({ task: taskA, tasks })
+const runPlugins = function({ task, secondArg }) {
+  return reduceAsync(PLUGINS, eRunPlugin, { task, secondArg }, mergePlugin)
+}
 
-  return plugin(task, { tasks, runTask: runTaskA })
+const runPlugin = function({ task, secondArg }, plugin) {
+  return plugin(task, secondArg)
 }
 
 // We merge the return value of each plugin
-const mergePlugin = function({ task, tasks }, taskA) {
-  const taskB = { ...task, ...taskA }
-  return { task: taskB, tasks }
+const mergePlugin = function({ task, secondArg }, taskA) {
+  return { task: { ...task, ...taskA }, secondArg }
 }
 
 // Add initial `task` to every thrown error
-const eRunPlugins = addGenErrorHandler(runPlugins, ({ task }) => ({ task }))
+const eRunPlugins = addGenErrorHandler(runPlugins, ({ task: { taskKey } }) => ({ task: taskKey }))
 
 // Add `rawRequest` and `rawResponse` (named `request` and `response`) to every
 // thrown error, if available
-const eRunPlugin = addGenErrorHandler(runPlugin, ({ rawRequest, rawResponse }) => ({
+const eRunPlugin = addGenErrorHandler(runPlugin, ({ task: { rawRequest, rawResponse } }) => ({
   request: rawRequest,
   response: rawResponse,
 }))

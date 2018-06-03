b57773c2cfcf9cdb196ae98f76ea19ee9213fd1a
ehmicky <ehmicky@gmail.com>
2018-06-03 17:00:00 +0100

    Order reporting

--- src/modules/report/complete.js
+++ src/modules/report/complete.js
@@ -2,9 +2,63 @@
 
 const { callReporters } = require('./call')
 
-// Reporting for each task
+// Reporting for each task.
+// We ensure reporting output has same order as tasks definition.
+// We do so by buffering each task until its reporting time comes.
 const complete = async function(input) {
-  const { config } = input
+  const {
+    config,
+    config: {
+      report,
+      report: { taskKeys, inputs, index },
+    },
+  } = input
+
+  const { key } = getTask(input)
+
+  // Save current task's result (i.e. reporting input)
+  // `config.report.inputs|index` are stateful and directly mutated because
+  // they need to be shared between parallel tasks
+  inputs[key] = input
+
+  // Only use keys not reported yet
+  const keys = taskKeys.slice(index)
+
+  // Retrieve how many tasks should now be unbuffered
+  const count = getCount({ keys, inputs })
+
+  // Update index to last reported task
+  report.index += count
+
+  // Unbuffer tasks, i.e. report them
+  await completeTasks({ count, keys, inputs, config })
+}
+
+const getTask = function({ task, error }) {
+  if (task !== undefined) {
+    return task
+  }
+
+  return error.task
+}
+
+const getCount = function({ keys, inputs }) {
+  const count = keys.findIndex(key => inputs[key] === undefined)
+
+  if (count === -1) {
+    return keys.length
+  }
+
+  return count
+}
+
+const completeTasks = async function({ count, keys, inputs, config }) {
+  const promises = keys.slice(0, count).map(key => completeTask({ key, inputs, config }))
+  await Promise.all(promises)
+}
+
+const completeTask = async function({ key, inputs, config }) {
+  const input = inputs[key]
   await callReporters({ config, input, type: 'complete' })
 }
 
--- src/modules/report/start/main.js
+++ src/modules/report/start/main.js
@@ -17,7 +17,15 @@ const start = async function(config) {
 
   await callReporters({ config: { report: reportC }, input: config, type: 'start' })
 
-  return { report: reportC }
+  const reportD = addOrdering({ config, report: reportC })
+
+  return { report: reportD }
+}
+
+// Used to ensure tasks ordering
+const addOrdering = function({ config: { tasks }, report }) {
+  const taskKeys = tasks.map(({ key }) => key)
+  return { ...report, taskKeys, inputs: {}, index: 0 }
 }
 
 module.exports = {

481be490dafbc6eca86d17681f670088235ca83d
ehmicky <ehmicky@gmail.com>
2018-06-03 17:00:00 +0100

    Add TAP plan

--- src/modules/tap/start/main.js
+++ src/modules/tap/start/main.js
@@ -5,10 +5,12 @@ const { tap } = require('../../../utils')
 const { getOutput } = require('./output')
 
 // Start TAP v13 output
-const startTap = async function({ tap: tapConfig }) {
+const startTap = async function({ tap: tapConfig, tasks }) {
   const output = await getOutput({ tapConfig })
 
   tap.version({ output })
+
+  tap.plan(tasks.length, { output })
 }
 
 module.exports = {
--- src/utils/tap.js
+++ src/utils/tap.js
@@ -6,6 +6,23 @@ const version = function(opts) {
   write('TAP version 13', opts)
 }
 
+const plan = function(integer, opts) {
+  if (!Number.isInteger(integer)) {
+    throw new Error(`tap.plan() argument must be an integer, not ${integer}`)
+  }
+
+  const planString = getPlan({ integer })
+  write(planString, opts)
+}
+
+const getPlan = function({ integer }) {
+  if (integer === 0) {
+    return '0..0'
+  }
+
+  return `1..${integer}`
+}
+
 const write = function(string, { output = stdout } = {}) {
   output.write(`${string}\n`)
 }
@@ -13,5 +30,6 @@ const write = function(string, { output = stdout } = {}) {
 module.exports = {
   tap: {
     version,
+    plan,
   },
 }

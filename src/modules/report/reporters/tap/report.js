f584452b383ff434dd71840235e64db01c885b4d
ehmicky <ehmicky@gmail.com>
2018-06-03 17:00:00 +0100

    Fix default for config.report.options.colors

--- src/modules/report/reporters/tap/report.js
+++ src/modules/report/reporters/tap/report.js
@@ -3,7 +3,7 @@
 const colorize = require('tap-colorize')
 
 // TAP reporter
-const report = function({ options: { colors = true } }) {
+const report = function({ options: { colors = false } }) {
   if (colors === false) {
     return
   }

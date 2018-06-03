c795b2253302408ddacf6bccfe9d7821473712e6
ehmicky <ehmicky@gmail.com>
2018-06-03 17:00:00 +0100

    Finish fixing requiredness

--- src/modules/random/task.js
+++ src/modules/random/task.js
@@ -2,7 +2,7 @@
 
 const jsonSchemaFaker = require('json-schema-faker')
 
-const { isSameParam } = require('../../utils')
+const { isSkippedOptional, fixRequired, addOptionalsProbability } = require('./optional')
 
 // Generates random values based on `task.random.*` JSON schemas
 const generateParams = function({ call: { params, ...call } }) {
@@ -13,7 +13,7 @@ const generateParams = function({ call: { params, ...call } }) {
 }
 
 // Generate value based on a single JSON schema
-const generateParam = function({ param, param: { random, value: schema }, params }) {
+const generateParam = function({ param, param: { random }, params }) {
   if (random === undefined) {
     return param
   }
@@ -22,29 +22,15 @@ const generateParam = function({ param, param: { random, value: schema }, params
     return
   }
 
+  const paramA = fixRequired({ param, params })
+
+  addOptionalsProbability({ param: paramA })
+
+  const { value: schema } = paramA
   const schemaA = fixArray({ schema })
 
-  addOptionalsProbability({ random })
-
   const value = jsonSchemaFaker(schemaA)
-  return { ...param, value }
-}
-
-// `params` with `random: optional` are only kept when merged with another
-// parameter. E.g. OpenAPI parameters that are `required: false` will only be
-// used if merged into some other `task.call|random.*` parameter.
-// This also means using an empty object in `task.call|random.*` allows re-using
-// spec parameters.
-const isSkippedOptional = function({ param, param: { random }, params }) {
-  return random === 'optional' && isNotMerged({ param, params })
-}
-
-const isNotMerged = function({ param, params }) {
-  return !params.some(paramB => willBeMerged(param, paramB))
-}
-
-const willBeMerged = function(paramA, paramB) {
-  return paramA !== paramB && isSameParam(paramA, paramB)
+  return { ...paramA, value }
 }
 
 // json-schema-faker does not work properly with array schema that do not have
@@ -55,23 +41,6 @@ const fixArray = function({ schema, schema: { type, items = {} } }) {
   }
 
   return { ...schema, items: { ...items, type: 'string' } }
-}
-
-// When `random` is `deep`, all deep properties are generated regardless of `required`.
-// E.g. `task.call|random.*` are always deeply generated because they are
-// explicited by the user.
-// When `random` is `shallow` or `optional`, `required` is used to determine
-// whether deep properties should be geneated.
-// E.g. `spec` parameters use this.
-const addOptionalsProbability = function({ random }) {
-  const optionalsProbability = PROBABILITY[random]
-  jsonSchemaFaker.option({ optionalsProbability })
-}
-
-const PROBABILITY = {
-  deep: 1,
-  shallow: 0,
-  optional: 0,
 }
 
 jsonSchemaFaker.option({

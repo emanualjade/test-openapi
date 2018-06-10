047ee567c2495331bbf13ff00481db28e100c1a9
ehmicky <ehmicky@gmail.com>
2018-06-10 17:00:00 +0200

    Improve highlight theme

--- src/modules/report/utils/colors.js
+++ src/modules/report/utils/colors.js
@@ -3,14 +3,19 @@
 const chalk = require('chalk')
 
 // Colors used in reporting
-const { red, dim, yellow } = chalk
+const { reset, red, dim, yellow, magenta, italic, bold, inverse } = chalk
 const orange = chalk.rgb(250, 100, 50)
 const grey = chalk.rgb(150, 150, 150)
 
 module.exports = {
+  reset,
   red,
   dim,
   grey,
   yellow,
+  magenta,
   orange,
+  italic,
+  bold,
+  inverse,
 }
--- src/modules/report/utils/highlight.js
+++ src/modules/report/utils/highlight.js
@@ -2,6 +2,8 @@
 
 const { highlight, highlightAuto } = require('emphasize')
 
+const { reset, red, dim, grey, yellow, magenta, italic, bold, inverse } = require('./colors')
+
 // Syntax highlighting
 const highlightValue = function({ string, value, format }) {
   // Not performed on strings, unless `format` was specified
@@ -12,13 +14,60 @@ const highlightValue = function({ string, value, format }) {
   // Non-strings are highlighted as YAML, unless `errorProp.format` was explicited
   const formatA = format || 'yaml'
 
-  return highlight(formatA, string).value
+  return highlight(formatA, string, THEME).value
 }
 
 // Console (ANSI sequences) syntax color highlighting
 // Automatically guesses MIME type/format
 const highlightValueAuto = function(string) {
-  return highlightAuto(string).value
+  return highlightAuto(string, THEME).value
+}
+
+// Theme colors
+const THEME = {
+  comment: grey,
+  quote: grey,
+
+  symbol: dim,
+  bullet: dim,
+  subst: dim,
+  meta: dim,
+  'meta keyword': dim,
+  'selector-attr': dim,
+  'selector-pseudo': dim,
+  link: dim,
+
+  string: reset,
+
+  keyword: magenta,
+  'selector-tag': magenta,
+  addition: magenta,
+
+  number: magenta,
+  'meta meta-string': magenta,
+  literal: magenta,
+  doctag: magenta,
+  regexp: magenta,
+
+  title: magenta,
+  section: magenta,
+  name: magenta,
+  'selector-id': magenta,
+  'selector-class': magenta,
+
+  attribute: yellow,
+  attr: yellow,
+  variable: yellow,
+  'template-variable': yellow,
+  'class title': yellow,
+  type: yellow,
+
+  built_in: red,
+  deletion: red,
+
+  emphasis: italic,
+  strong: bold,
+  formula: inverse,
 }
 
 module.exports = {

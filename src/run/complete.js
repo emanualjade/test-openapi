8a657a7010becd6cb3ad2b9e71abdd7bb9a5e6ae
ehmicky <ehmicky@gmail.com>
2018-06-15 16:00:00 +0100

    Fix silent errors in complete handlers

--- src/run/complete.js
+++ src/run/complete.js
@@ -1,16 +1,21 @@
 'use strict'
 
+const { addErrorHandler } = require('../errors')
 const { runHandlers } = require('../plugins')
 
 // Run each `plugin.complete()`
 const completeTask = async function({ task, originalTask, plugins, config }) {
-  await runHandlers({}, plugins, 'complete', { task, originalTask, config }, completePluginHandler)
+  await runHandlers({}, plugins, 'complete', { task, originalTask, config })
+
+  return task
 }
 
-const completePluginHandler = function(error, { task }) {
+const completeTaskHandler = function(error, { task }) {
   return { ...task, error }
 }
 
+const eCompleteTask = addErrorHandler(completeTask, completeTaskHandler)
+
 module.exports = {
-  completeTask,
+  completeTask: eCompleteTask,
 }
--- src/run/main.js
+++ src/run/main.js
@@ -65,9 +65,9 @@ const fireTasks = function({ config, config: { tasks }, mRunTask, plugins }) {
 const fireTask = async function({ task, task: { originalTask }, config, mRunTask, plugins }) {
   const taskA = await bootTask({ task, config, mRunTask, plugins })
 
-  await completeTask({ task: taskA, originalTask, plugins, config })
+  const taskB = await completeTask({ task: taskA, originalTask, plugins, config })
 
-  return taskA
+  return taskB
 }
 
 // The following plugins can be run (in order).

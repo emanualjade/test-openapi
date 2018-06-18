e7f552f3f274ce89fada243d8b9adf5896bdf220
ehmicky <ehmicky@gmail.com>
2018-06-18 11:00:00 +0100

    Move comment

--- src/plugins/config.js
+++ src/plugins/config.js
@@ -7,6 +7,27 @@ const getConfigPlugins = function({ config: { plugins, ...config } }) {
 }
 
 // Plugins always included
+// `start`, i.e. before all tasks:
+//   - `glob`: merge tasks whose name include globbing matching other task names.
+//   - `only`: check if `config|task.only` is used
+//   - `spec`: parse, validate and normalize an OpenAPI specification
+//   - `report`: start reporting
+//   - `repeat`: repeat each task `config.repeat` times
+// `task`, i.e. for each task:
+//   - `only`: select tasks according to `config|task.only`
+//   - `skip`: skip task if `task.skip: true`
+//   - `deps`: replace all `deps`, i.e. references to other tasks
+//   - `spec`: add OpenAPI specification to `task.random|validate.*`
+//   - `random`: generates random values based on `task.random.*` JSON schemas
+//   - `serialize`: stringify request parameters
+//   - `url`: build request URL from request parameters
+//   - `call`: fire actual HTTP call
+//   - `parse`: parse response
+//   - `validate`: validate response against `task.validate.*` JSON schemas
+// `complete`, i.e. after each tasks:
+//   - `report`: reporting for current task
+// `end`, i.e. after all tasks:
+//   - `report`: end of reporting
 const DEFAULT_PLUGINS = [
   'glob',
 

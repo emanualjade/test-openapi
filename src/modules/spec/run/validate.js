a4f237fcb7a703a2bab3f95506f06e18d3f6969e
ehmicky <ehmicky@gmail.com>
2018-06-19 16:25:43 +0200

    Default `validate.status` to first success status in spec

--- src/modules/spec/run/validate.js
+++ src/modules/spec/run/validate.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const { sortArray } = require('../../../utils')
+
 const { getSpecResponse } = require('./operation')
 
 // Add OpenAPI specification to `task.validate.*`
@@ -22,7 +24,25 @@ const addSpecToValidate = function({
     return validate
   }
 
-  return { ...validate, byStatus: { ...byStatus, ...specResponses } }
+  const status = getSpecStatus({ specResponses })
+
+  return { status, ...validate, byStatus: { ...byStatus, ...specResponses } }
+}
+
+// If there is a 2** response, the lowest one is used as default `validate.status`
+const getSpecStatus = function({ specResponses }) {
+  const statuses = Object.keys(specResponses)
+  const statusesA = sortArray(statuses)
+  const statusA = statusesA.find(isSuccessStatus)
+  if (statusA === undefined) {
+    return
+  }
+
+  return Number(statusA)
+}
+
+const isSuccessStatus = function(status) {
+  return status.startsWith('2')
 }
 
 module.exports = {

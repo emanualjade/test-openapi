99f4eb4d5d82e02e9c76055f7dc12986adb9d043
ehmicky <ehmicky@gmail.com>
2018-06-20 19:35:32 +0200

    Add promise utilities

--- src/utils/promise.js
+++ src/utils/promise.js
@@ -4,13 +4,35 @@
 // As opposed to them, this does not create a new promise callback if the
 // return value is synchronous, i.e. it avoids unnecessary new microtasks
 const promiseThen = function(retVal, func) {
-  if (!retVal || typeof retVal.then !== 'function') {
+  if (!isPromise(retVal)) {
     return func(retVal)
   }
 
   return retVal.then(func)
 }
 
+// Similar to `await Promise.all([...retVals])` or `Promise.all([...retVals]).then()`
+// As opposed to them, this does not create a new promise callback if the
+// return value is synchronous, i.e. it avoids unnecessary new microtasks
+const promiseAllThen = function(retVals, func) {
+  const retValsA = promiseAll(retVals)
+  return promiseThen(retValsA, func)
+}
+
+const promiseAll = function(retVals) {
+  if (!retVals.some(isPromise)) {
+    return retVals
+  }
+
+  return Promise.all(retVals)
+}
+
+const isPromise = function(retVal) {
+  return retVal && typeof retVal.then === 'function'
+}
+
 module.exports = {
   promiseThen,
+  promiseAllThen,
+  promiseAll,
 }

d0fa2e7796c0f1664910745a83a8f3bf8c9a0673
ehmicky <ehmicky@gmail.com>
2018-06-21 13:39:04 +0200

    Add comments

--- src/helpers/var.js
+++ src/helpers/var.js
@@ -2,8 +2,13 @@
 
 const { get } = require('lodash')
 
+// `{ $var: 'varName' }` helper
+// Replaced by `config.helpers.$var.varName: value`
 const varHelper = function(path, { options }) {
-  return get(options, path)
+  // If either `path` is not present in `options` or if the value is `undefined`,
+  // the returned value will be `undefined`
+  const value = get(options, path)
+  return value
 }
 
 module.exports = varHelper

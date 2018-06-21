cff3d137664095871381082a6471f057ad1eb258
ehmicky <ehmicky@gmail.com>
2018-06-22 00:21:54 +0200

    Improve recursive error handling

--- src/run/run.js
+++ src/run/run.js
@@ -12,7 +12,7 @@ const runTask = async function({ task, config, plugins, nestedPath }) {
 }
 
 const runAll = function({ task, config, plugins, nestedPath }) {
-  const runTaskA = eRecursiveRunTask.bind(null, { config, plugins, nestedPath })
+  const runTaskA = recursiveRunTask.bind(null, { config, plugins, nestedPath })
 
   return runHandlers(
     'run',
@@ -25,73 +25,66 @@ const runAll = function({ task, config, plugins, nestedPath }) {
   )
 }
 
-// Pass simplified `runTask()` for recursive tasks
-// Tasks can use `nestedPath` to know if this is a recursive call
-// As opposed to regular `runTask()`, failed task throws.
-const recursiveRunTask = function(
-  { config, plugins, nestedPath },
-  { task, task: { key }, property, self },
-) {
-  const nestedPathA = appendNestedPath({ nestedPath, key, property, self })
+// Top-level errors are returned as `task.error`
+// From `error: { task }` to `task: { error }`
+const runAllHandler = function(error) {
+  const { task } = error
 
-  checkRecursion({ nestedPath: nestedPathA })
+  delete error.task
+  task.error = error
 
-  return runTask({ task, config, plugins, nestedPath: nestedPathA })
+  return task
 }
 
-// Add `error.path` to every thrown error
-const recursiveRunTaskHandler = function(
-  error,
-  { config, plugins, nestedPath },
-  { task: { key }, property, self, getError },
-) {
-  const nestedPathA = appendNestedPath({ nestedPath, key, property, self })
+const eRunAll = addErrorHandler(runAll, runAllHandler)
 
-  // When `getError()` is specified, we throw that error instead but with
-  // `error.nested` set to the nested task.
-  // When `getError()` is undefined, the nested error is propagated as is.
-  if (getError === undefined) {
-    return nonNestedTaskHandler({ error, nestedPath: nestedPathA })
-  }
-
-  return nestedTaskHandler({ error, nestedPath: nestedPathA, getError, config, plugins })
-}
-
-const nonNestedTaskHandler = function({ error, nestedPath }) {
-  // Only keep deepest `error.path`
-  if (error.path === undefined && nestedPath.length > 0) {
-    error.path = nestedPath
+// Error handler for each plugin handler
+// We want to rememeber the current task on the first handler that throws.
+// We do this by attaching it to `error.task`, then extracting it on a top-level
+// error handler.
+// The error is finally set to `task.error`
+const runPluginHandler = function(error, task) {
+  // Recursive tasks already have `error.task` defined
+  if (error.task === undefined) {
+    error.task = task
   }
 
   throw error
 }
 
-const nestedTaskHandler = function({ error, nestedPath, getError, config, plugins }) {
-  // Each nested task recreated a new error, ensuring `error.task|plugin|etc.`
-  // are set each time
-  const topError = getError()
-
-  topError.nested = getNestedError({ error, nestedPath, config, plugins })
-
-  throw topError
+// Returning `done: true` in any `run` handler stops the iteration but without
+// errors (as opposed to throwing an exception)
+// This implies successful tasks might be emptier than expected.
+// This is used e.g. by `skip|only` or `repeat` plugins
+const stopOnDone = function({ done }) {
+  return done
 }
 
-const getNestedError = function({ error, error: { nested }, nestedPath, config, plugins }) {
-  // Only keep deepest task as `error.nested`
-  if (nested !== undefined) {
-    return nested
+// Pass simplified `runTask()` for recursive tasks
+// Tasks can use `nestedPath` to know if this is a recursive call
+// As opposed to regular `runTask()`, failed task throws.
+const recursiveRunTask = async function(
+  { config, plugins, nestedPath },
+  { task, task: { key }, property, self, getError },
+) {
+  const nestedPathA = appendNestedPath({ nestedPath, key, property, self })
+
+  checkRecursion({ nestedPath: nestedPathA })
+
+  const taskA = await runTask({ task, config, plugins, nestedPath: nestedPathA })
+
+  const { error } = taskA
+
+  if (error === undefined) {
+    return taskA
   }
 
-  // This will only be set to the deepest `error.path`
-  error.path = nestedPath
+  if (getError === undefined) {
+    throwNonNestedError({ error, nestedPath: nestedPathA })
+  }
 
-  const nestedA = errorToTask({ error })
-  const nestedB = getTaskReturn({ task: nestedA, config, plugins })
-  nestedB.error = convertPlainObject(nestedB.error)
-  return nestedB
+  throwNestedError({ task: taskA, nestedPath: nestedPathA, getError })
 }
-
-const eRecursiveRunTask = addErrorHandler(recursiveRunTask, recursiveRunTaskHandler)
 
 const appendNestedPath = function({ nestedPath = [], key, property, self }) {
   // `nestedPath` is unchanged if `self: true`
@@ -133,46 +126,47 @@ const checkRecursion = function({ nestedPath }) {
   throw new TestOpenApiError(`Infinite task recursion`)
 }
 
-const runAllHandler = function(error, { nestedPath }) {
-  // Nested `runTask()` errors are propagated
-  if (nestedPath !== undefined) {
-    throw error
-  }
-
-  // Top-level errors are returned as `task.error`
-  return errorToTask({ error })
-}
-
-// From `error: { task }` to `task: { error }`
-const errorToTask = function({ error, error: { task } }) {
-  delete error.task
-  task.error = error
-
-  return task
-}
-
-const eRunAll = addErrorHandler(runAll, runAllHandler)
-
-// Error handler for each plugin handler
-// We want to rememeber the current task on the first handler that throws.
-// We do this by attaching it to `error.task`, then extracting it on a top-level
-// error handler.
-// The error is finally set to `task.error`
-const runPluginHandler = function(error, task) {
-  // Recursive tasks already have `error.task` defined
-  if (error.task === undefined) {
-    error.task = task
+// When `getError()` is undefined, the nested error is propagated as is.
+const throwNonNestedError = function({ error, nestedPath }) {
+  // Only keep deepest `error.path`
+  if (error.path === undefined && nestedPath.length > 0) {
+    error.path = nestedPath
   }
 
   throw error
 }
 
-// Returning `done: true` in any `run` handler stops the iteration but without
-// errors (as opposed to throwing an exception)
-// This implies successful tasks might be emptier than expected.
-// This is used e.g. by `skip|only` or `repeat` plugins
-const stopOnDone = function({ done }) {
-  return done
+// When `getError()` is specified, we throw that error instead but with
+// `error.nested` set to the nested task.
+const throwNestedError = function({ task, nestedPath, getError }) {
+  // Each nested task recreated a new error, ensuring `error.task|plugin|etc.`
+  // are set each time
+  const topError = getError()
+
+  topError.nested = getNestedError({ task, nestedPath })
+
+  throw topError
+}
+
+const getNestedError = function({
+  task,
+  task: {
+    error,
+    error: { nested },
+  },
+  nestedPath,
+}) {
+  // Only keep deepest task as `error.nested`
+  if (nested !== undefined) {
+    return nested
+  }
+
+  const errorA = convertPlainObject(error)
+
+  // This will only be set to the deepest `error.path`
+  const errorB = { ...errorA, path: nestedPath }
+
+  return { ...task, error: errorB }
 }
 
 module.exports = {

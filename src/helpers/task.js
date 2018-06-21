3b61b7f9175905e61ca917eec68c380c28dd5306
ehmicky <ehmicky@gmail.com>
2018-06-21 15:22:01 +0200

    Add $task helper

---
+++ src/helpers/task.js
@@ -0,0 +1,55 @@
+'use strict'
+
+const { get } = require('lodash')
+
+const { TestOpenApiError } = require('../errors')
+
+// `{ $task: 'TASK PATH [OPT,...]' }` helper
+// Runs TASK and replace helper with `task[PATH]`
+const taskHelper = async function(taskArg, { config: { tasks } }, { runTask }) {
+  const { taskKey, path, options } = parseTaskArg({ taskArg })
+
+  const task = findTask({ taskKey, tasks })
+
+  const taskA = await runTask(task)
+
+  const taskProp = await getTaskProp({ task: taskA, path, options })
+  return taskProp
+}
+
+// Parse 'TASK PATH [OPTS,...]'
+const parseTaskArg = function({ taskArg }) {
+  const [taskKey, path, options = ''] = taskArg.split(/\s+/)
+
+  if (!taskKey || !path) {
+    throw new TestOpenApiError('argument must be a task name followed by a property path')
+  }
+
+  const optionsA = options.split(',')
+
+  return { taskKey, path, options: optionsA }
+}
+
+// Find the task definition
+const findTask = function({ taskKey, tasks }) {
+  const task = tasks.find(({ key }) => key === taskKey)
+
+  if (task === undefined) {
+    throw new TestOpenApiError(`task '${taskKey}' does not exist`)
+  }
+
+  return task
+}
+
+// Retrieve task property
+const getTaskProp = function({ task, task: { key }, path, options }) {
+  const taskProp = get(task, path)
+
+  if (taskProp === undefined && !options.includes('optional')) {
+    throw new TestOpenApiError(`task '${key}' did not return any property '${path}'`)
+  }
+
+  return taskProp
+}
+
+module.exports = taskHelper

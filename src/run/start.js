ece9d0c2b2e3bf3642fee24af1c2273db07b7cba
ehmicky <ehmicky@gmail.com>
2018-07-09 11:00:00 +0100

    Do not allow modifying `tasks` anymore in `start` handlers

--- src/run/start.js
+++ src/run/start.js
@@ -3,8 +3,10 @@
 const { runHandlers } = require('../plugins')
 
 // Run each `plugin.start()`
-const startTasks = function({ config, plugins }) {
-  return runHandlers('start', plugins, config)
+const startTasks = async function({ config, config: { tasks }, plugins }) {
+  const configA = await runHandlers('start', plugins, config)
+  // Only allow modifying `tasks` in `plugin.load()`
+  return { ...configA, tasks }
 }
 
 module.exports = {

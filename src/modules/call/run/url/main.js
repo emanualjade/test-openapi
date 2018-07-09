886964ce47d79c254841ec324860b51e3e0f0546
ehmicky <ehmicky@gmail.com>
2018-07-09 11:00:00 +0100

    Use `context.helpers()` in `call` plugin

--- src/modules/call/run/url/main.js
+++ src/modules/call/run/url/main.js
@@ -7,20 +7,20 @@ const { normalizeUrl } = require('./normalize')
 const { addQueryParams } = require('./query')
 
 // Build request URL from request parameters
-const addUrl = function({ call, call: { request, rawRequest } = {} }, { config }) {
+const addUrl = function({ call, call: { request, rawRequest } = {} }, { config, helpers }) {
   if (call === undefined) {
     return
   }
 
-  const url = getFullUrl({ rawRequest, config })
+  const url = getFullUrl({ rawRequest, config, helpers })
   const rawRequestA = { ...rawRequest, url }
   const requestA = { ...request, url }
 
   return { call: { ...call, rawRequest: rawRequestA, request: requestA } }
 }
 
-const getFullUrl = function({ rawRequest, config }) {
-  const url = getServer({ rawRequest, config })
+const getFullUrl = function({ rawRequest, config, helpers }) {
+  const url = getServer({ rawRequest, config, helpers })
   const urlA = addPath({ url, rawRequest })
   const urlB = addUrlParams({ url: urlA, rawRequest })
   const urlC = normalizeUrl({ url: urlB })

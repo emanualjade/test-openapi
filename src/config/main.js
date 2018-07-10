140521cf8f5e024fbbe09b48d3781afa2a400de9
ehmicky <ehmicky@gmail.com>
2018-07-10 11:58:47 +0200

    Fix `undefined` config values

--- src/config/main.js
+++ src/config/main.js
@@ -1,14 +1,18 @@
 'use strict'
 
+const { omitBy } = require('lodash')
+
 const { validateConfig } = require('./validate')
 const DEFAULT_CONFIG = require('./defaults')
 
 // Load and normalize configuration
 const loadConfig = function({ config }) {
-  validateConfig({ config })
+  const configA = omitBy(config, value => value === undefined)
+
+  validateConfig({ config: configA })
 
   // Apply default values
-  const configB = { ...DEFAULT_CONFIG, ...config }
+  const configB = { ...DEFAULT_CONFIG, ...configA }
 
   return configB
 }

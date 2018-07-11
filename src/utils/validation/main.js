7bd5ba9fd93d978eed8eeae4b40ac6ec94a4185a
ehmicky <ehmicky@gmail.com>
2018-07-11 17:00:00 +0200

    Improve JSON schema validation

--- src/utils/validation/main.js
+++ src/utils/validation/main.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const memoize = require('fast-memoize')
+const { get } = require('lodash')
 
 const { validator } = require('./validator')
 
@@ -12,20 +13,54 @@ const validateFromSchema = function({ schema, value, name }) {
     return {}
   }
 
-  const {
-    errors: [error],
-  } = validator
-  const { dataPath } = error
-  const errorA = getError({ error, name })
-
-  const path = dataPath.replace(/^./, '')
-
-  return { error: errorA, path }
+  const error = getError({ validator, schema, value, name })
+  return error
 }
 
-// Human-friendly error
-const getError = function({ error, name = '' }) {
+const getError = function({
+  validator: {
+    errors: [error],
+  },
+  schema,
+  value,
+  name,
+}) {
+  const message = getErrorMessage({ error, name })
+  const path = getErrorPath({ error })
+  const valueA = getErrorValue({ path, value })
+  const schemaPath = getErrorSchemaPath({ error })
+  const schemaA = getErrorSchema({ schemaPath, schema })
+
+  return { error: message, path, value: valueA, schemaPath, schema: schemaA }
+}
+
+const getErrorMessage = function({ error, name = '' }) {
   return validator.errorsText([error], { dataVar: name })
+}
+
+const getErrorPath = function({ error: { dataPath } }) {
+  return dataPath.replace(INDEX_BRACKETS_REGEXP, '.$1').replace(/^\./, '')
+}
+
+// Array index: `[integer]`
+const INDEX_BRACKETS_REGEXP = /\[([\d]+)\]/g
+
+const getErrorValue = function({ path, value }) {
+  if (path === '') {
+    return value
+  }
+
+  return get(value, path)
+}
+
+const getErrorSchemaPath = function({ error: { schemaPath } }) {
+  return schemaPath.replace('#/', '').split('/')
+}
+
+const getErrorSchema = function({ schemaPath, schema }) {
+  const key = schemaPath[schemaPath.length - 1]
+  const value = get(schema, schemaPath)
+  return { [key]: value }
 }
 
 // Compilation is automatically memoized by `ajv` but not validation

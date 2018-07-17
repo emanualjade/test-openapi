844ca3332cfa29d9406dd6f93be3efa518c2fe32
ehmicky <ehmicky@gmail.com>
2018-07-17 17:00:00 +0200

    Allow custom merge

--- src/utils/merge.js
+++ src/utils/merge.js
@@ -7,12 +7,26 @@ const merge = function(...objects) {
   return mergeWith({}, ...objects, shallowMergeArray)
 }
 
-const shallowMergeArray = function(dest, src) {
-  if (Array.isArray(dest)) {
-    return src
+// Allow customizing merge by adding a `func`
+const customMerge = function(func, ...objects) {
+  return mergeWith({}, ...objects, customMergeValue.bind(null, func))
+}
+
+const customMergeValue = function(func, src, dest) {
+  if (Array.isArray(src)) {
+    return dest
+  }
+
+  return func(src, dest)
+}
+
+const shallowMergeArray = function(src, dest) {
+  if (Array.isArray(src)) {
+    return dest
   }
 }
 
 module.exports = {
   merge,
+  customMerge,
 }

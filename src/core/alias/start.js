f2dcb7683f217a87abf2a8699d487d8a2f26cf84
ehmicky <ehmicky@gmail.com>
2018-07-18 17:00:00 +0200

    Fix compatibility between `only` and `alias`

--- src/core/alias/start.js
+++ src/core/alias/start.js
@@ -7,8 +7,8 @@ const { getEvalTask } = require('./eval')
 // `task.alias.$$NAME: '[PATH] [OPTS]'` allows using `$$NAME` in any task, to
 // run the task that defined the alias, and retrieve a specific property at `PATH`
 // It does so by adding those helpers to `startData.helpers`.
-const start = function({ helpers }, { config: { tasks } }) {
-  const aliasHelpers = tasks.map(getTaskHelpers)
+const start = function({ helpers }, { config: { _allTasks: allTasks } }) {
+  const aliasHelpers = allTasks.map(getTaskHelpers)
   const aliasHelpersA = Object.assign({}, ...aliasHelpers)
 
   return { helpers: { ...helpers, ...aliasHelpersA } }

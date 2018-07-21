60fbadcacc33b35111abd35e9975b3501ab95d48
ehmicky <ehmicky@gmail.com>
2018-07-21 17:00:00 +0200

    Error handling of `plugin.helpers()`

--- src/core/helpers/run/plugin.js
+++ src/core/helpers/run/plugin.js
@@ -1,17 +1,24 @@
 'use strict'
 
+const { addErrorHandler } = require('../../../errors')
+
 // Retrieve all `plugin.helpers`
 const getPluginsHelpers = function({ plugins, task, context }) {
-  const pluginHelpers = plugins.map(plugin => getPluginHelper({ plugin, task, context }))
+  const pluginHelpers = plugins.map(plugin => getPluginHelpers({ plugin, task, context }))
   const pluginHelpersA = Object.assign({}, ...pluginHelpers)
   return pluginHelpersA
 }
 
-const getPluginHelper = function({ plugin: { helpers }, task, context }) {
+const getPluginHelpers = function({ plugin, plugin: { helpers }, task, context }) {
   if (helpers === undefined) {
     return
   }
 
+  const helpersA = eGetHelpers({ plugin, task, context })
+  return helpersA
+}
+
+const getHelpers = function({ plugin: { helpers }, task, context }) {
   if (typeof helpers !== 'function') {
     return helpers
   }
@@ -19,6 +26,19 @@ const getPluginHelper = function({ plugin: { helpers }, task, context }) {
   const helpersA = helpers(task, context)
   return helpersA
 }
+
+// Add `error.message|module` when `plugin.helpers` throws
+const getHelpersHandler = function(error, { plugin: { name } }) {
+  error.message = `Error while retrieving 'plugin.helpers': ${error.message}`
+
+  if (error.module === undefined) {
+    error.module = `plugin-${name}`
+  }
+
+  throw error
+}
+
+const eGetHelpers = addErrorHandler(getHelpers, getHelpersHandler)
 
 module.exports = {
   getPluginsHelpers,

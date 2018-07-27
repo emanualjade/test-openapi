2cddd91a19e1012b639a53d0c5075b0ee2bfa468
ehmicky <ehmicky@gmail.com>
2018-07-27 17:00:00 +0200

    Remove `null` handling in `invalid`

--- src/core/spec/run/invalid.js
+++ src/core/spec/run/invalid.js
@@ -24,34 +24,10 @@ const getInvalidParam = function({ param }) {
     return {}
   }
 
-  const type = addNullType({ param })
-
   // TODO: json-schema-faker support for the `not` keyword is lacking
   // E.g. `type` array is not supported, so `invalid value` actually does not work
   // at the moment.
-  return { not: { ...param, type } }
-}
-
-// When using 'invalid value', we want to make sure the value is generated, i.e. it
-// should never be `null`
-// If `param` is `undefined`, nothing to negate, but it should still be generated,
-// i.e. will return `['null']`
-// TODO: use `{ allOf: [{ not: { enum: [null] } }, ...] }` instead
-// This is unfortunately not supported at the moment by json-schema-faker
-const addNullType = function({ param: { type = [] } = {} }) {
-  if (type === 'null') {
-    return type
-  }
-
-  if (!Array.isArray(type)) {
-    return ['null', type]
-  }
-
-  if (type.includes('null')) {
-    return type
-  }
-
-  return ['null', ...type]
+  return { not: param }
 }
 
 module.exports = {

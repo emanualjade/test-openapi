19ef89c3aeda4acdb09012386dcfa338b6f0a8ba
ehmicky <ehmicky@gmail.com>
2018-07-27 15:36:56 +0200

    Add comment

--- src/core/spec/start/normalize/json_schema.js
+++ src/core/spec/start/normalize/json_schema.js
@@ -5,6 +5,7 @@ const { omit } = require('lodash')
 
 // OpenAPI schemas are not 100% valid JSON schemas v4, so we use a library to convert them
 const normalizeSchema = function({ schema }) {
+  // At the moment, this function should not throw because we already validated `schema`
   const schemaA = openapiToJsonSchema(schema)
 
   // `ajv` complains about it

ae8bc50ceac7509c8204719be4ec4ef9cfa6c99e
ehmicky <ehmicky@gmail.com>
2018-07-27 17:00:00 +0200

    Remove `null` handling in call serializer

--- src/core/call/run/serialize/main.js
+++ src/core/call/run/serialize/main.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const { omitBy, mapValues } = require('lodash')
+const { mapValues } = require('lodash')
 
 const { keyToLocation, stringifyFlat } = require('../../../../utils')
 const { findBodyHandler } = require('../../body')
@@ -14,31 +14,15 @@ const serialize = function({ call }) {
     return
   }
 
-  const callA = removeNull({ call })
+  const callA = normalizeContentType({ call })
 
-  const callB = normalizeContentType({ call: callA })
+  const callB = normalizeMethod({ call: callA })
 
-  const callC = normalizeMethod({ call: callB })
-
-  const request = normalizeTimeout({ call: callC })
+  const request = normalizeTimeout({ call: callB })
 
   const rawRequest = mapValues(request, stringifyParam)
 
   return { call: { ...call, request, rawRequest } }
-}
-
-// Specifying `null` means 'do not send this parameter'.
-// Only applies to top-level value, i.e. should never be an issue.
-// This is useful for:
-//  - removing parameters specified by another plugin, i.e. removing parameters
-//    specified by `spec` plugin
-//  - distinguishing from `?queryVar` (empty string) and no `queryVar` (null)
-//  - being consistent with `validate` plugin, which use `null` to specify
-//    'should not be defined'
-// When used with `$$random`, parameters can be randomly generated or not
-// using `type: ['null', ...]`
-const removeNull = function({ call }) {
-  return omitBy(call, value => value === null)
 }
 
 const normalizeTimeout = function({ call: { timeout = DEFAULT_TIMEOUT, ...call } }) {

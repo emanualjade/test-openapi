4469f6e55979cea36bdae084530fce6e76ea0ab8
ehmicky <ehmicky@gmail.com>
2018-07-29 17:00:00 +0200

    Allow `x-oneOf`

--- src/core/spec/start/normalize/json_schema.js
+++ src/core/spec/start/normalize/json_schema.js
@@ -7,7 +7,7 @@ const { crawl } = require('../../../../utils')
 
 // OpenAPI schemas are not 100% valid JSON schemas v4, so we use a library to convert them
 const normalizeSchema = function({ schema }) {
-  const schemaA = crawl(schema, undefined, { evalKey: replaceNullable })
+  const schemaA = normalizeKeys({ schema })
 
   // At the moment, this function should not throw because we already validated `schema`
   const schemaB = openapiToJsonSchema(schemaA)
@@ -18,16 +18,28 @@ const normalizeSchema = function({ schema }) {
   return schemaC
 }
 
-// OpenAPI 3 `nullable` does not exist in OpenAPI 2, but it's very useful as
-// it is converted to `type: ['null', ...]` by `openapi-schema-to-json-schema`.
-// We allow it under OpenAPI 2 using `x-nullable` which is used by other libraries
-// like Redoc.
-const replaceNullable = function(key) {
-  if (key !== 'x-nullable') {
-    return key
+const normalizeKeys = function({ schema }) {
+  return crawl(schema, undefined, { evalKey: normalizeKey })
+}
+
+const normalizeKey = function(key) {
+  if (KEYS[key] !== undefined) {
+    return KEYS[key]
   }
 
-  return 'nullable'
+  return key
+}
+
+const KEYS = {
+  // OpenAPI 3 `nullable` does not exist in OpenAPI 2, but it's very useful as
+  // it is converted to `type: ['null', ...]` by `openapi-schema-to-json-schema`.
+  // We allow it under OpenAPI 2 using `x-nullable` which is used by other libraries
+  // like Redoc.
+  'x-nullable': 'nullable',
+  // OpenAPI 3 `oneOf` does not exist in OpenAPI 2, but it's very useful because
+  // it allows alternatives of `type`,
+  // We allow it under OpenAPI 2 using `x-oneOf`.
+  'x-oneOf': 'oneOf',
 }
 
 module.exports = {

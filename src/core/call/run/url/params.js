0c5a56c4542902058ca4dcd0956b5a72956ebe2a
ehmicky <ehmicky@gmail.com>
2018-07-30 17:00:00 +0200

    Use Express-style variables in `call.path`, not OpenAPI

--- src/core/call/run/url/params.js
+++ src/core/call/run/url/params.js
@@ -1,29 +1,95 @@
 'use strict'
 
-const { getPath } = require('../../../../utils')
-const { TestOpenApiError } = require('../../../../errors')
+const { parse, tokensToFunction } = require('path-to-regexp')
 
-// Replace `url` request parameters to the request URL
-// Can replace `{...}` in both `task.call.server` and `task.call.path`
+const { removePrefixes, getPath } = require('../../../../utils')
+const { addErrorHandler, TestOpenApiError } = require('../../../../errors')
+
+// Replace `url` request parameters to the request URL.
+// Can replace in both `task.call.server` and `task.call.path`
+// Uses same syntax as Express paths, e.g. `:NAME`, `:NAME*`, `:NAME+` or `(RegExp)`
+// The library calls `encodeURIComponent()` on each URL variable
 const addUrlParams = function({ url, rawRequest }) {
-  return url.replace(URL_PARAM_REGEXP, (urlA, name) => getUrlParam({ name, rawRequest }))
+  const urlParams = removePrefixes(rawRequest, 'url')
+
+  const tokens = parseUrl({ url })
+
+  validateRequiredParams({ tokens, urlParams })
+
+  const urlA = serializeUrl({ tokens, urlParams })
+  return urlA
 }
 
-// Matches `url` request parameters, e.g. `/model/{id}`
-// It's quite loose because the OpenAPI specification does not specify
-// which characters are allowed in `url` request parameter names
-const URL_PARAM_REGEXP = /\{([^}]+)\}/g
+// Parse URL `:NAME` variables tokens
+const parseUrl = function({ url }) {
+  const tokens = parse(url)
+  const tokensA = tokens.map(handlePort)
+  return tokensA
+}
 
-const getUrlParam = function({ name, rawRequest }) {
-  const nameA = `url.${name}`
-  const property = getPath(['task', 'call', nameA])
-  const value = rawRequest[nameA]
-
-  if (value === undefined) {
-    throw new TestOpenApiError(`The URL parameter '${name}' must be defined`, { property })
+// `path-to-regex` considers `:PORT` to be a URL variable, which is incorrect.
+// We fix this by serializing tokes back to a plain string.
+const handlePort = function(token) {
+  if (typeof token === 'string') {
+    return token
   }
 
-  return value
+  // Not if `:NUMBER*` nor `:NUMBER+`
+  const { name, optional, repeat } = token
+  const isPort = !optional && !repeat && PORT_REGEXP.test(name)
+  if (!isPort) {
+    return token
+  }
+
+  return `:${name}`
+}
+
+const PORT_REGEXP = /^\d+$/
+
+// `path-to-regex` already validates required parameters, but we do first to
+// provide a better error message
+const validateRequiredParams = function({ tokens, urlParams }) {
+  tokens.forEach(token => validateRequiredParam({ token, urlParams }))
+}
+
+const validateRequiredParam = function({ token, urlParams }) {
+  if (!isRequiredParam(token)) {
+    return
+  }
+
+  const { name } = token
+
+  if (urlParams[name] !== undefined && urlParams[name] !== '') {
+    return
+  }
+
+  throwError('must not be empty', { name })
+}
+
+const isRequiredParam = function(token) {
+  return typeof token !== 'string' && !token.optional
+}
+
+// Replace URL `:NAME` tokens by `call.url.*` values
+const serializeUrl = function({ tokens, urlParams }) {
+  // We run `tokensToFunction` on each `token` instead of once on all of them
+  // so the error handler knows which `token` failed without parsing the error message
+  return tokens.map(token => eSerializeToken({ token, urlParams })).join('')
+}
+
+const serializeToken = function({ token, urlParams }) {
+  return tokensToFunction([token])(urlParams)
+}
+
+const serializeTokenHandler = function({ message }, { token: { name } }) {
+  throwError(`is invalid: ${message}`, { name })
+}
+
+const eSerializeToken = addErrorHandler(serializeToken, serializeTokenHandler)
+
+const throwError = function(message, { name }) {
+  const property = getPath(['task', 'call', `url.${name}`])
+  throw new TestOpenApiError(`The URL parameter '${name}' ${message}`, { property })
 }
 
 module.exports = {
--- src/core/spec/start/normalize/params/constants.js
+++ src/core/spec/start/normalize/params/constants.js
@@ -1,14 +1,17 @@
 'use strict'
 
+const { TestOpenApiError } = require('../../../../../errors')
+
 // Operation's method, server and path as a `task.call.method|server|path` parameter
 const getConstants = function({ spec, method, path }) {
   const serverParam = getServerParam({ spec })
-  const methodParam = getConstant({ value: method, key: 'method' })
-  const pathParam = getConstant({ value: path, key: 'path' })
+  const methodParam = getMethodParam({ method })
+  const pathParam = getPathParam({ path })
 
   return { ...serverParam, ...methodParam, ...pathParam }
 }
 
+// Retrieve `task.call.server`
 const getServerParam = function({ spec: { host: hostname, basePath } }) {
   // Only if OpenAPI `host` is defined
   if (hostname === undefined) {
@@ -19,6 +22,44 @@ const getServerParam = function({ spec: { host: hostname, basePath } }) {
   const value = `http://${hostname}${basePath}`
   return getConstant({ value, key: 'server' })
 }
+
+// Retrieve `task.call.method`
+const getMethodParam = function({ method }) {
+  return getConstant({ value: method, key: 'method' })
+}
+
+// Retrieve `task.call.path`
+const getPathParam = function({ path }) {
+  const value = getExpressPath({ path })
+  const pathParam = getConstant({ value, key: 'path' })
+  return pathParam
+}
+
+// Transform an OpenAPI path `/path/{variable}` into an Express-style path `/path/:variable`
+// Note that according to OpenAPI spec, path variables are always required.
+const getExpressPath = function({ path }) {
+  return path.replace(URL_PARAM_REGEXP, (match, name) => getExpressVariable({ name, path }))
+}
+
+// Matches `url` request parameters, e.g. `/model/{id}`
+// It's quite loose because the OpenAPI specification does not specify
+// which characters are allowed in `url` request parameter names
+const URL_PARAM_REGEXP = /\{([^}]+)\}/g
+
+const getExpressVariable = function({ name, path }) {
+  if (VALID_EXPRESS_PATH_NAME.test(name)) {
+    return `:${name}`
+  }
+
+  throw new TestOpenApiError(
+    `In OpenAPI 'path' '${path}', variable name '${name}' is invalid: it must only contain letters, digits and underscores`,
+  )
+}
+
+// Valid path variable name according to `path-to-regexp` library.
+// We are sligtly more restrictive as we disallow starting with a digit,
+// to distinguish from URL port.
+const VALID_EXPRESS_PATH_NAME = /^[a-zA-Z_]\w*$/
 
 const getConstant = function({ value, key }) {
   return { [key]: { type: 'string', enum: [value] } }

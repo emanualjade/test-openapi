5b5fd18c706cbcf9c359004e4e70cdfea3c55a05
ehmicky <ehmicky@gmail.com>
2018-08-06 17:00:00 +0200

    Improve `validate.status`

--- src/core/validate/run/status.js
+++ src/core/validate/run/status.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const { STATUS_CODES } = require('statuses')
-const { difference, uniq } = require('lodash')
+const { difference, uniq, groupBy } = require('lodash')
 
 const { TestOpenApiError } = require('../../../errors')
 const { getWordsList, sortArray } = require('../../../utils')
@@ -17,9 +17,8 @@ const validateStatus = function({
     return
   }
 
-  const vStatusesStr = getWordsList(vStatuses)
-  const expected = normalizeStatuses(vStatuses)
-  throw new TestOpenApiError(`Status code should not be ${status} but ${vStatusesStr}`, {
+  const { value: expected, statusesStr } = normalizeStatuses(vStatuses)
+  throw new TestOpenApiError(`Status code should not be ${status} but ${statusesStr}`, {
     value: status,
     expected,
     property: 'task.validate.status',
@@ -49,42 +48,61 @@ const parseStatus = function(status) {
   return statusesD
 }
 
-const checkValidStatuses = function({ statuses }) {
-  const invalidStatuses = difference(statuses, VALID_STATUSES)
-  if (invalidStatuses.length === 0) {
-    return
-  }
-
-  const invalidStatusesStr = getWordsList(invalidStatuses, { op: 'and' })
-  const value = normalizeStatuses(invalidStatuses)
-  const expected = VALID_STATUSES.map(Number)
-  throw new TestOpenApiError(
-    `The task definition is invalid: those are not valid HTTP status codes: ${invalidStatusesStr}`,
-    { value, expected, property: 'task.validate.status' },
-  )
-}
-
 // `validate.status` can be `1xx`, `2xx`, `3xx`, `4xx` or `5xx`, case-insensitively
 const parseRange = function(status) {
   if (!RANGE_REGEXP.test(status)) {
     return status
   }
 
-  const firstNum = status[0]
-  const statuses = VALID_STATUSES.filter(validStatus => validStatus.startsWith(firstNum))
+  const statuses = VALID_STATUSES_MAP[status.toLowerCase()]
   return statuses
 }
 
 const RANGE_REGEXP = /^[1-5]xx$/i
-const VALID_STATUSES = Object.keys(STATUS_CODES)
+
+const checkValidStatuses = function({ statuses }) {
+  const invalidStatuses = difference(statuses, VALID_STATUSES)
+  if (invalidStatuses.length === 0) {
+    return
+  }
+
+  const { value, statusesStr } = normalizeStatuses(invalidStatuses)
+  const expected = VALID_STATUSES.map(Number)
+  throw new TestOpenApiError(
+    `The task definition is invalid: those are not valid HTTP status codes: ${statusesStr}`,
+    { value, expected, property: 'task.validate.status' },
+  )
+}
 
 // Normalize status for error reporting
 const normalizeStatuses = function(statuses) {
-  if (statuses.length === 1) {
-    return numerizeStatus(statuses[0])
+  const statusesA = replaceByRanges({ statuses })
+
+  const statusesB = sortArray(statusesA)
+
+  const statusesC = statusesB.map(numerizeStatus)
+
+  if (statusesC.length === 1) {
+    return { value: statusesC[0], statusesStr: String(statusesC[0]) }
   }
 
-  return statuses.map(numerizeStatus)
+  const statusesStr = getWordsList(statusesC)
+  return { value: statusesC, statusesStr }
+}
+
+// Replace `100` + `101` + `102` by `1xx`, for any status code range
+const replaceByRanges = function({ statuses }) {
+  return Object.entries(VALID_STATUSES_MAP).reduce(replaceByRange, statuses)
+}
+
+const replaceByRange = function(statuses, [range, rangeStatuses]) {
+  const statusesA = difference(statuses, rangeStatuses)
+  // Only if all possible status codes for that range are here
+  if (statuses.length - statusesA.length !== rangeStatuses.length) {
+    return statuses
+  }
+
+  return [range, ...statusesA]
 }
 
 // Try to error report statuses as integers
@@ -97,6 +115,19 @@ const numerizeStatus = function(status) {
 
   return statusA
 }
+
+// All possible HTTP status code as an array and as a
+// `{ "1xx": [...], "2xx": [...], ... }` map
+const getValidStatusesMap = function() {
+  const validStatuses = Object.keys(STATUS_CODES)
+  const validStatusesMap = groupBy(validStatuses, status => `${status[0]}xx`)
+  return { validStatuses, validStatusesMap }
+}
+
+const {
+  validStatuses: VALID_STATUSES,
+  validStatusesMap: VALID_STATUSES_MAP,
+} = getValidStatusesMap()
 
 module.exports = {
   validateStatus,

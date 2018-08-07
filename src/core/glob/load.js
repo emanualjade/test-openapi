6f4c3a37f6daf9a181bedbbedf1fc7b77e0a5509
ehmicky <ehmicky@gmail.com>
2018-08-07 17:00:00 +0200

    Fix globing priority order

--- src/core/glob/load.js
+++ src/core/glob/load.js
@@ -29,11 +29,55 @@ const mergeGlob = function({ task, globTasks }) {
     return task
   }
 
-  return merge({}, ...globTasksA, task)
+  return merge(...globTasksA, task)
 }
 
-const findGlobTasks = function({ task: { key }, globTasks }) {
-  return globTasks.filter(({ key: taskPattern }) => isMatch(key, taskPattern))
+const findGlobTasks = function({ task: { key, path }, globTasks }) {
+  return globTasks
+    .filter(({ key: taskPattern }) => isMatch(key, taskPattern))
+    .sort((taskA, taskB) => compareGlobTasks({ taskA, taskB, path }))
+}
+
+// Compute which glob tasks have priority over each other.
+// Mostly depends on the file it was loaded in with priority:
+//   same file > no file > other files in alphabetical order
+// Within the same file, globbing tasks declared last in keys order have priority.
+// Note that we do not use `config.tasks` order as globbing expansion order is not stable.
+const compareGlobTasks = function({ taskA: { path: pathA }, taskB: { path: pathB }, path }) {
+  // Inside the same file, we use object keys order, i.e. globbing tasks declared
+  // last have priority.
+  // Object keys order is not very reliable, so we must make sure `tasks` does not
+  // change order since tasks loading.
+  // Returning 0 will let `Array.sort()` rely on array order instead.
+  if (pathA === pathB) {
+    return 0
+  }
+
+  // Globbing tasks within the same file have priority
+  if (pathA === path) {
+    return 1
+  }
+
+  if (pathB === path) {
+    return -1
+  }
+
+  // Globbing tasks within no file (i.e. passed directly as objects) come next
+  if (pathA === undefined) {
+    return 1
+  }
+
+  if (pathB === undefined) {
+    return -1
+  }
+
+  // We then rely on task file name alphabetical order.
+  // Tasks whose filenames is closer to z have higher priority.
+  if (pathA < pathB) {
+    return -1
+  }
+
+  return 1
 }
 
 module.exports = {

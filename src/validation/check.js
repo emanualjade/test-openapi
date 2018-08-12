42605b421ffef62f1961dacce9f113cd5ab72e86
ehmicky <ehmicky@gmail.com>
2018-08-12 17:00:00 +0200

    Fix JSON validation for undefined values

--- src/validation/check.js
+++ src/validation/check.js
@@ -1,6 +1,9 @@
 'use strict'
 
+const { omitBy } = require('lodash')
+
 const { TestOpenApiError, BugError } = require('../errors')
+const { crawl, isObject } = require('../utils')
 
 const { validateFromSchema } = require('./validate')
 
@@ -8,15 +11,10 @@ const { validateFromSchema } = require('./validate')
 // `error.value|schema|property` set accordingly
 // As opposed to `validateFromSchema()` which is meant to be separated in its
 // own repository, this is meant only for this project.
-// Values whose key is defined but whose value is `undefined` `opts.value` are
-// mostly ignored by ajv except:
-//  - if top-level `opts.value` itself is `undefined`
-//  - `additionalProperties` and `propertyNames`, but this should be ok since
-//    it validates the key not the value
-//  - `minProperties|maxProperties`: this is problematic as `undefined` should
-//    behave as if the key was not defined. But we'll be ok with it for the moment.
-const checkSchema = function({ bug = false, ...opts }) {
-  const error = validateFromSchema(opts)
+const checkSchema = function({ bug = false, value, ...opts }) {
+  const valueA = removeUndefined({ value })
+
+  const error = validateFromSchema({ ...opts, value: valueA })
   if (error === undefined) {
     return
   }
@@ -26,6 +24,25 @@ const checkSchema = function({ bug = false, ...opts }) {
   const errorProps = getErrorProps({ opts, error })
 
   throw new ErrorType(message, errorProps)
+}
+
+// Values whose key is defined but whose value is `undefined` `opts.value` are
+// mostly ignored by ajv except:
+//  - if top-level `opts.value` itself is `undefined`
+//  - `propertyNames`: this should be ok since it validates the key not the value
+//  - `minProperties|maxProperties|patternProperties|additionalProperties`:
+//    this is problematic as `undefined` should behave as if the key was not defined.
+// I.e. we remove `undefined` values deeply
+const removeUndefined = function({ value }) {
+  return crawl(value, removeUndefinedProp)
+}
+
+const removeUndefinedProp = function(value) {
+  if (!isObject(value)) {
+    return value
+  }
+
+  return omitBy(value, valueA => valueA === undefined)
 }
 
 const getErrorProps = function({

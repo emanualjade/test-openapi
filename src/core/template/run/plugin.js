cd2f2be754789d0df7b34c97eb373532fbf4e545
ehmicky <ehmicky@gmail.com>
2018-08-15 17:00:00 +0200

    Adjust `plugin.template` priority order

--- src/core/template/run/plugin.js
+++ src/core/template/run/plugin.js
@@ -8,11 +8,14 @@ const { wrapTemplateVars } = require('./check')
 
 // Retrieve all `plugin.template`
 const getPluginsVars = function({ context, context: { _plugins: plugins } }) {
-  const pluginsVarsMap = plugins.map(plugin => getPluginVars({ plugin, context }))
-  const pluginsVarsMapA = Object.assign({}, ...pluginsVarsMap)
+  const pluginsVarsMap = getPluginsVarsMap({ context, plugins })
+  const pluginsVars = mergePluginsVars({ plugins, pluginsVarsMap })
+  return { pluginsVars, pluginsVarsMap }
+}
 
-  const pluginsVars = Object.assign({}, ...Object.values(pluginsVarsMapA))
-  return { pluginsVars, pluginsVarsMap: pluginsVarsMapA }
+const getPluginsVarsMap = function({ context, plugins }) {
+  const pluginsVarsMap = plugins.map(plugin => getPluginVars({ plugin, context }))
+  return Object.assign({}, ...pluginsVarsMap)
 }
 
 const getPluginVars = function({ plugin, plugin: { name, template }, context }) {
@@ -67,6 +70,17 @@ const validateVarName = function({ name, plugin }) {
     `'plugin.template' returned a template variable with an invalid name '${name}': it must be prefixed with $$ and only contain letters, digits, underscores and dashes`,
     { value: name, property, module },
   )
+}
+
+const mergePluginsVars = function({ plugins, pluginsVarsMap }) {
+  // Ensure `plugin.template` merge priority follows `plugins` order
+  const pluginsVars = plugins
+    .filter(({ name }) => name !== 'template')
+    .map(({ name }) => pluginsVarsMap[name])
+    .reverse()
+  // Core template variables always have least priority
+  const pluginsVarsA = Object.assign({}, pluginsVarsMap.template, ...pluginsVars)
+  return pluginsVarsA
 }
 
 module.exports = {

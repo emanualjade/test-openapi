20c4695830773971773b5392339ea4eb3070cb13
ehmicky <ehmicky@gmail.com>
2018-08-17 17:00:00 +0200

    Fix error output serialization

--- src/serialize/output.js
+++ src/serialize/output.js
@@ -1,26 +1,24 @@
 'use strict'
 
 const { crawl } = require('../utils')
+const { convertPlainObject } = require('../errors')
 
-const { convertTaskError } = require('./error')
 const { addSerializeFail } = require('./fail')
 const { isJsonType, getMessage, UNDEFINED, ESCAPED_UNDEFINED } = require('./common')
 
 // Applied on tasks output, i.e. what is reported and returned
 const serializeOutput = function({ task, plugins }) {
-  const taskA = convertTaskError({ task })
-
   // We use a `state` object because `crawl` utility does not allow returning both
   // the crawled object and extra information
   const state = {}
 
-  const taskB = crawl(taskA, (value, path) => serializeOutputValue({ value, path, state }), {
+  const taskA = crawl(task, (value, path) => serializeOutputValue({ value, path, state }), {
     topDown: true,
   })
 
   const { error } = state
-  const taskC = addSerializeFail({ task: taskB, error, plugins })
-  return taskC
+  const taskB = addSerializeFail({ task: taskA, error, plugins })
+  return taskB
 }
 
 const serializeOutputValue = function({ value, path, state }) {
@@ -38,6 +36,10 @@ const serializeOutputValue = function({ value, path, state }) {
 
   if (typeof value === 'function') {
     return serializeFunction(value)
+  }
+
+  if (value instanceof Error) {
+    return convertPlainObject(value)
   }
 
   // If the value cannot be serialized, returns the first one as `error`.

8e0bf60fcba38d1b0c846ad01f279c74fe36c15a
ehmicky <ehmicky@gmail.com>
2018-10-05 15:00:00 +0100

    Improve `keepFuncName()`

--- src/utils/func_name.js
+++ src/utils/func_name.js
@@ -1,13 +1,25 @@
 'use strict'
 
-// Wraps a functor so it does not modify a function name
-// Function names are e.g. used by performance monitoring
+// Wraps a functor so it does not modify a function `name`, `length`, etc.
 const keepFuncName = function(functor) {
-  return (func, ...args) => {
-    const funcA = functor(func, ...args)
-    Object.defineProperty(funcA, 'name', { value: func.name })
-    return funcA
+  return (originalFunc, ...args) => {
+    const wrappedFunc = functor(originalFunc, ...args)
+
+    copyProperties({ originalFunc, wrappedFunc })
+
+    return wrappedFunc
   }
+}
+
+const copyProperties = function({ originalFunc, wrappedFunc }) {
+  Reflect.ownKeys(originalFunc).forEach(propName =>
+    copyProperty({ originalFunc, wrappedFunc, propName }),
+  )
+}
+
+const copyProperty = function({ originalFunc, wrappedFunc, propName }) {
+  const prop = Object.getOwnPropertyDescriptor(originalFunc, propName)
+  Object.defineProperty(wrappedFunc, propName, prop)
 }
 
 module.exports = {

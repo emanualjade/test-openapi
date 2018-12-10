c24dc922234bab62f93368262ec16f7b0c39a747
ehmicky <ehmicky@gmail.com>
2018-12-10 14:45:06 +0100

    Fix linting

--- gulp/tasks/test.js
+++ gulp/tasks/test.js
@@ -8,7 +8,7 @@ const gulpExeca = require('../exec')
 // We do not use `gulp-eslint` because it does not support --cache
 // This task also fixes linting errors and apply `prettier` code formatting
 const lint = function() {
-  const files = FILES.SOURCE.join(' ')
+  const files = FILES.SOURCE.map(pattern => `'${pattern}'`).join(' ')
   return gulpExeca(
     `eslint ${files} --ignore-path .gitignore --fix --cache --format codeframe --max-warnings 0 --report-unused-disable-directives`,
   )

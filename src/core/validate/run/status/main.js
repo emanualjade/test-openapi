73e81c8e40d5b43dc7ab43b74ffb521a493d9ac5
ehmicky <ehmicky@gmail.com>
2019-01-31 17:00:00 +0100

    Fix error message for HTTP status

--- src/core/validate/run/status/main.js
+++ src/core/validate/run/status/main.js
@@ -18,7 +18,7 @@ const validateStatus = function({
 
   const { value: expected, statusesStr } = normalizeStatuses(vStatuses)
   throw new TestOpenApiError(
-    `Status code was expected to be ${status}, but got ${statusesStr}`,
+    `Status code was expected to be ${statusesStr}, but got ${status}`,
     { value: status, expected, property: PROPERTY },
   )
 }

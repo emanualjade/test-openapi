7bb595010757fe8b518e73d1b0075295855b738f
ehmicky <ehmicky@gmail.com>
2019-02-17 10:00:00 +0100

    Add smoke tests

--- test/index.js
+++ test/index.js
@@ -1,7 +1,14 @@
 'use strict'
 
 const test = require('ava')
+const execa = require('execa')
 
-test('Dummy test', t => {
-  t.pass()
+const BINARY_PATH = `${__dirname}/../bin/test_openapi.js`
+const TASKS_FILE = `${__dirname}/tasks.yml`
+
+test('Smoke test', async t => {
+  const { code, stdout } = await execa(BINARY_PATH, [TASKS_FILE], {
+    reject: false,
+  })
+  t.snapshot({ code, stdout })
 })

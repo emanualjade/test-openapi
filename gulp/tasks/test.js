3bf72f1fa548a0f8b5ee042a8adb958a20ab4e05
ehmicky <ehmicky@gmail.com>
2019-02-17 10:00:00 +0100

    Add gulp test

--- gulp/tasks/test.js
+++ gulp/tasks/test.js
@@ -5,8 +5,9 @@ const { series } = require('gulp')
 const { getWatchTask } = require('../utils')
 
 const { check } = require('./check')
+const { unit } = require('./unit')
 
-const testTask = series(check)
+const testTask = series(check, unit)
 
 // eslint-disable-next-line fp/no-mutation
 testTask.description = 'Lint and test source files'

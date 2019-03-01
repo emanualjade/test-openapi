00dfb237aba9e4155191deb2d404e3849c1fbea0
ehmicky <ehmicky@gmail.com>
2019-03-01 10:00:00 +0100

    Use new build task

--- test/index.js
+++ test/index.js
@@ -3,7 +3,7 @@
 const test = require('ava')
 const execa = require('execa')
 
-const BINARY_PATH = `${__dirname}/../dist/bin/index.js`
+const BINARY_PATH = `${__dirname}/../dist/src/bin/index.js`
 const TASKS_FILE = `${__dirname}/tasks.yml`
 
 test('Smoke test', async t => {

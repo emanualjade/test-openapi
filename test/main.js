d5f9fad2e11320cfe68bcaa010b6bff368258bb4
ehmicky <ehmicky@gmail.com>
2019-04-17 10:00:00 +0200

    Fix tests

--- test/main.js
+++ test/main.js
@@ -2,7 +2,7 @@ import test from 'ava'
 import execa from 'execa'
 
 const BINARY_PATH = `${__dirname}/../src/bin/main.js`
-const TASKS_FILE = `${__dirname}/tasks.json`
+const TASKS_FILE = `${__dirname}/tasks.yml`
 
 test('Smoke test', async t => {
   const { code, stdout } = await execa(BINARY_PATH, [TASKS_FILE], {

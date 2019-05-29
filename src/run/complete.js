300ae4beb3113ce068803e34b5fe8801174eb703
ehmicky <ehmicky@gmail.com>
2019-05-29 10:00:00 +0200

    Refactoring

--- src/run/complete.js
+++ src/run/complete.js
@@ -1,24 +1,14 @@
-import { addErrorHandler } from '../errors/handler.js'
 import { runHandlers } from '../plugins/handlers.js'
 
 // Run each `plugin.complete()`
-const completeTask = async function({ task, context, plugins }) {
-  // `task` cannot be modified
-  await runHandlers({
-    type: 'complete',
-    plugins,
-    input: task,
-    context,
-  })
-
-  return task
+export const completeTask = async function({ task, context, plugins }) {
+  try {
+    // `task` cannot be modified
+    await runHandlers({ type: 'complete', plugins, input: task, context })
+    return task
+  // Errors in `complete` handlers return `task.error`, just like the ones in
+  // `run` handlers
+  } catch (error) {
+    return { ...task, error }
+  }
 }
-
-// Errors in `complete` handlers return `task.error`, just like the ones in
-// `run` handlers
-const completeTaskHandler = function(error, { task }) {
-  return { ...task, error }
-}
-
-const eCompleteTask = addErrorHandler(completeTask, completeTaskHandler)
-export { eCompleteTask as completeTask }

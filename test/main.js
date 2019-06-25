c8bc474272bce11730df5f3e41aa3782d547ec7c
ehmicky <ehmicky@gmail.com>
2019-06-25 10:00:00 +0200

    Fix tests

--- test/main.js
+++ test/main.js
@@ -5,9 +5,9 @@ const BINARY_PATH = `${__dirname}/../src/bin/main.js`
 const TASKS_FILE = 'test/tasks.yml'
 
 test('Smoke test', async t => {
-  const { code, stdout } = await execa(BINARY_PATH, [TASKS_FILE], {
+  const { exitCode, stdout } = await execa(BINARY_PATH, [TASKS_FILE], {
     reject: false,
   })
   const stdoutA = stdout.replace(/User-Agent.*/u, '')
-  t.snapshot({ code, stdout: stdoutA })
+  t.snapshot({ exitCode, stdout: stdoutA })
 })

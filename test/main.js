1e2b6e2c36c4dee074022f5b3efb5bed73c43693
ehmicky <ehmicky@gmail.com>
2019-07-01 10:00:00 +0200

    Use get-bin-path

--- test/main.js
+++ test/main.js
@@ -1,11 +1,12 @@
 import test from 'ava'
 import execa from 'execa'
+import { getBinPath } from 'get-bin-path'
 
-const BINARY_PATH = `${__dirname}/../src/bin/main.js`
+const BINARY_PATH = getBinPath()
 const TASKS_FILE = 'test/tasks.yml'
 
 test('Smoke test', async t => {
-  const { exitCode, stdout } = await execa(BINARY_PATH, [TASKS_FILE], {
+  const { exitCode, stdout } = await execa(await BINARY_PATH, [TASKS_FILE], {
     reject: false,
   })
   const stdoutA = stdout.replace(/User-Agent.*/u, '')
